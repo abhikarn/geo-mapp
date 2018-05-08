63258e377dcb3c622ca6ced85d8ed02f1776ffe1
Admin <abhishek.karn@outlook.com>
2018-05-08 23:15:07 +0530

    Refactored School Master List

--- src/app/app-features/school-master/school-master-create/school-master-create.component.ts
+++ src/app/app-features/school-master/school-master-create/school-master-create.component.ts
@@ -9,39 +9,26 @@ import { AppBaseComponent, School, WebService, Masters } from '@app/app-core';
 export class SchoolMasterCreateComponent extends AppBaseComponent implements OnInit {
     school: School = { schoolName: '', source: 'web', status: 'Active', approved: true };
     masters: any;
-    schoolLst: School[];
-    schools: School[];
     masterConst: any = Masters;
     currentPage = 0;
+    editMode = false;
     constructor(private router: Router, private activatedRoute: ActivatedRoute, private webService: WebService) {
         super();
         this.masters = this.activatedRoute.parent.snapshot.data.masters;
     }
 
     ngOnInit() {
-        console.log(this.activatedRoute.snapshot.data.schoolLst);
-        this.schoolLst = this.activatedRoute.snapshot.data.schoolLst || [];
-        this.schools = this.activatedRoute.snapshot.data.schoolLst || [];
-        this.school = this.activatedRoute.snapshot.data.school;
-        console.log(this.school);
+        this.editMode = !!this.activatedRoute.snapshot.params.id;
+        if (this.editMode) {
+            this.school = this.activatedRoute.snapshot.data.school;
+        }
     }
 
     saveSchoolMaster() {
-        console.log(this.school);
-        this.webService.saveSchoolMaster(this.school).subscribe((res) => {
-            console.log(res);
-            this.schoolLst = res;
-            this.router.navigate(['engage']);
+        this.webService.saveSchoolMaster(this.school).subscribe(() => {
+            this.router.navigate(['engage/school-master']);
         });
     }
-
-    doEdit(school: School) {
-        this.school = school;
-    }
-
-    updatePage(event) {
-        this.currentPage = +event.page;
-        this.schoolLst = this.schools.slice(event.first, +event.first + +event.rows);
-    }
 }
+
 
