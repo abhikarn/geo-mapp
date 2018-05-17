db1187bf56dcfb0adb0407e92dec71e0c11bf356
Admin <abhishek.karn@outlook.com>
2018-05-17 23:33:20 +0530

    removed unwanted code

--- src/app/app-features/geo-heirarchy/geo-heirarchy-mapping/geo-heirarchy-mapping.component.ts
+++ src/app/app-features/geo-heirarchy/geo-heirarchy-mapping/geo-heirarchy-mapping.component.ts
@@ -28,8 +28,6 @@ export class GeoHeirarchyMapComponent extends AppBaseComponent implements OnInit
   ) {
     super(componentFactoryResolver, router);
     this.masters = this.activatedRoute.parent.snapshot.data.masters;
-    console.log(this.masters);
-
   }
 
   ngOnInit() {
@@ -48,42 +46,9 @@ export class GeoHeirarchyMapComponent extends AppBaseComponent implements OnInit
     }
   }
 
-  // private showModalPopup(componentD: any, type: string, message: string) {
-  //   let childComponent = this.componentFactoryResolver.resolveComponentFactory(componentD);
-  //   let componentRef = this.parent.createComponent(childComponent);
-  //   (<any>componentRef.instance).modalConfig = { message: message };
-  //   setTimeout(() => {
-  //     (<any>componentRef.instance).showModal();
-  //     if (type === 'confirm') {
-  //       (<any>componentRef.instance).onReject.subscribe(() => {
-  //         // (<SaveDialogComponent>componentRef.instance).onAccept.unsubscribe();
-  //         componentRef.destroy();
-  //         childComponent = null;
-  //       });
-  //     }
-  //     (<any>componentRef.instance).onAccept.subscribe(() => {
-  //       // (<SaveDialogComponent>componentRef.instance).onAccept.unsubscribe();
-  //       componentRef.destroy();
-  //       childComponent = null;
-  //       this.router.navigate(['engage']);
-  //     });
-  //   }, 300);
-  // }
-
-  // private confirm() {
-  //   this.confirmationService.confirm({
-  //     message: 'The details are not saved . Are you sure you want to exit?',
-  //     accept: () => {
-  //       this.router.navigate(['engage']);
-  //     }
-  //   });
-  // }
-
   onOptionsSelected(event: string, source: string) {
     if (source === 'country') {
-      console.log(event);
       this.webService.getZones(parseInt(event, 10)).subscribe((res) => {
-        console.log(res);
         this.masters.zoneMaster = res;
         this.masters.branchMaster = null;
         this.masters.stateMaster = null;
@@ -93,9 +58,7 @@ export class GeoHeirarchyMapComponent extends AppBaseComponent implements OnInit
       });
     }
     if (source === 'zone') {
-      console.log(event);
       this.webService.getBranches(parseInt(event, 10)).subscribe((res) => {
-        console.log(res);
         this.masters.branchMaster = res;
         this.masters.stateMaster = null;
         this.masters.supervisorMaster = null;
@@ -104,17 +67,13 @@ export class GeoHeirarchyMapComponent extends AppBaseComponent implements OnInit
       });
     }
     if (source === 'branch') {
-      console.log(event);
       this.webService.getStates(parseInt(event, 10)).subscribe((res) => {
-        console.log(res);
         this.masters.stateMaster = res;
       });
     }
     if (source === 'state') {
-      console.log(event);
       this.webService.getRoleBasedUser('Supervisor', this.geoMapping.countryId, parseInt(event, 10))
         .subscribe((res) => {
-          console.log(res);
           this.supervisorModels = res;
         });
     }
@@ -122,7 +81,6 @@ export class GeoHeirarchyMapComponent extends AppBaseComponent implements OnInit
     if (source === 'super') {
       this.webService.getRoleBasedUser('Marketing User', this.geoMapping.countryId, this.geoMapping.stateId)
         .subscribe((res) => {
-          console.log(res);
           this.hierarchyUserModels = res;
         });
     }
@@ -130,33 +88,21 @@ export class GeoHeirarchyMapComponent extends AppBaseComponent implements OnInit
 
   onCancelReset() {
     this.showModalPopup('confirm', 'Are you sure you want to leave this page ?', 'engage');
-
-    // this.confirm();
   }
 
   saveData() {
-    console.log(this.geoMapping);
     this.webService.saveGeoHierarchy(this.geoMapping).subscribe((res) => {
-      console.log(res);
       this.showModalPopup('success', 'The mapping saved successfully', 'engage');
-      // this.saveConfirm();
-      // this.router.navigate(['engage']);
     });
   }
 
   saveConfirm() {
-    // this.confirmationService.confirm({
-    //   message: 'The details saved successfully',
-    //   accept: () => {
     this.router.navigate(['engage']);
-    //   }
-    // });
   }
 
   getMarketingUser() {
     this.webService.getRoleBasedUser('Marketing User', this.geoMapping.countryId, this.geoMapping.stateId)
       .subscribe((res) => {
-        console.log(res);
         this.hierarchyUserModels = res;
       });
   }
